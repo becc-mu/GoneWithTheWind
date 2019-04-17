68d05052cfa130d07140ae5e1466d1b385b3d42f
becc-mu <becc.mulugeta@gmail.com>
2019-04-18 00:24:36 +0100

    passing search book by title method

--- src/main/scala/com/company/library/Library.scala
+++ src/main/scala/com/company/library/Library.scala
@@ -1,24 +1,33 @@
 package com.company.library
 
+import scala.collection.mutable
+
 object Library {
 
 
   def main(args: Array[String]): Unit = {
-    val books = Books.all:List[Book]
+    //val books = Books.all:List[Book]
     //println(books)
 
-    def searchByTitle(title: String) = ???
+    def searchByTitle(title: String): Unit = {
+      val list = mutable.MutableList[Book]()
+      for(a <- Books.all) {
+        if(title != "" && a.title.contains(title)) {
+          list += a
+        }
+      }
+    }
 
 
-    def searchByAuthor() = ???
-
-    def searchByIsbn() = ???
-
-    def lendBooks() = ???
-
-    def notLendRefBooks() = ???
-
-    def bookIsAvaialable() = ???
+//    def searchByAuthor() = ???
+//
+//    def searchByIsbn() = ???
+//
+//    def lendBooks() = ???
+//
+//    def notLendRefBooks() = ???
+//
+//    def bookIsAvaialable() = ???
 
   }
 }
